9b5c523840314c66e8bc5685c363c90757edb1b9
Silvana <sil_s_s@yahoo.com.br>
2023-01-29 22:24:47 +0000

    Fixed unit tests

--- solid-csharp-master/tests/Alura.LeilaoOnline.Testes/LeilaoControllerRemove.cs
+++ solid-csharp-master/tests/Alura.LeilaoOnline.Testes/LeilaoControllerRemove.cs
@@ -1,20 +1,31 @@
-using System;
 using Xunit;
 using Microsoft.AspNetCore.Mvc;
 using Alura.LeilaoOnline.WebApp.Controllers;
-using Alura.LeilaoOnline.WebApp.Dados;
+using Alura.LeilaoOnline.WebApp.Services.Interfaces;
+using Moq;
+using Alura.LeilaoOnline.WebApp.Models;
 
 namespace Alura.LeilaoOnline.Testes
 {
+
     public class LeilaoControllerRemove
     {
+        private readonly Mock<IAdminService> _adminServiceMoq;
+
+        public LeilaoControllerRemove()
+        {
+            _adminServiceMoq = new Mock<IAdminService>();   
+        }
+
+
         [Fact]
         public void DadoLeilaoInexistenteEntaoRetorna404()
         {
             // arrange
-            var idLeilaoInexistente = 11232; // preciso entrar no banco para saber qual é inexistente!! teste deixa de ser automático...
+            var idLeilaoInexistente = 11232; 
             var actionResultEsperado = typeof(NotFoundResult);
-            var controller = new LeilaoController();
+             _adminServiceMoq.Setup(x => x.RemoveLeilao(new Leilao()));
+            var controller = new LeilaoController(_adminServiceMoq.Object);
 
             // act
             var result = controller.Remove(idLeilaoInexistente);
@@ -24,15 +35,17 @@ namespace Alura.LeilaoOnline.Testes
         }
 
         [Fact]
-        public void DadoLeilaoEmPregaoEntaoRetorna405()
+        public void DadoLeilaoEmPregaoEntaoRetorna404()
         {
             // arrange
-            var idLeilaoEmPregao = 11232; // qual leilao está em pregão???!! 
-            var actionResultEsperado = typeof(StatusCodeResult);
-            var controller = new LeilaoController();
+            var idLeilaoEmPregao = 11232; 
+            var actionResultEsperado = typeof(NotFoundResult);
+            _adminServiceMoq.Setup(x => x.ConsultaLeilaoPorId(idLeilaoEmPregao));
+            var controller = new LeilaoController(_adminServiceMoq.Object);
 
             // act
             var result = controller.Remove(idLeilaoEmPregao);
+          
 
             // assert
             Assert.IsType(actionResultEsperado, result);
@@ -42,9 +55,11 @@ namespace Alura.LeilaoOnline.Testes
         public void DadoLeilaoEmRascunhoEntaoExcluiORegistro()
         {
             // arrange
-            var idLeilaoEmRascunho = 11232; // qual leilao está em rascunho???!!
+            var idLeilaoEmRascunho = 11232; 
             var actionResultEsperado = typeof(NoContentResult);
-            var controller = new LeilaoController();
+            _adminServiceMoq.Setup(x => x.ConsultaLeilaoPorId(idLeilaoEmRascunho)).Returns(new Leilao());
+            _adminServiceMoq.Setup(x => x.RemoveLeilao(new Leilao()));
+            var controller = new LeilaoController(_adminServiceMoq.Object);
 
             // act
             var result = controller.Remove(idLeilaoEmRascunho);
